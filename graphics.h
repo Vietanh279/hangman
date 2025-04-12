17e2ea423dea644ec77a9ba689d8e55620cfaeaf
Vietanh279 <24020021@vnu.edu.vn>
2025-04-12 13:22:09 +0700

    Dohoa.h

--- graphics.h
+++ graphics.h
@@ -1,72 +1,72 @@
-#ifndef _GRAPHICS_H
-#define _GRAPHICS_H
+#ifndef _DOHOA_H
+#define _DOHOA_H
 using namespace std;
-#include "defs.h"
+#include "Cauhinh.h"
 #include <SDL_image.h>
 
-struct Graphics {
+struct DoHoa {
     SDL_Window* window;
     SDL_Renderer* renderer;
     TTF_Font* font;
-    SDL_Texture* hangmanStages[7];
+    SDL_Texture* HoatAnh[7];
 
     void init() {
         if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-            logErrorAndExit("SDL_Init", SDL_GetError());
+            KiemtraLoi("SDL_Init", SDL_GetError());
         }
-        window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        window = SDL_CreateWindow("Hangman",SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,  800, 600, SDL_WINDOW_SHOWN);
         if (window == nullptr) {
-            logErrorAndExit("CreateWindow", SDL_GetError());
+            KiemtraLoi("CreateWindow", SDL_GetError());
         }
         renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
         if (renderer == nullptr) {
-            logErrorAndExit("CreateRenderer", SDL_GetError());
+            KiemtraLoi("CreateRenderer", SDL_GetError());
         }
         if (TTF_Init() == -1) {
-            logErrorAndExit("TTF_Init", TTF_GetError());
+            KiemtraLoi("TTF_Init", TTF_GetError());
         }
         font = TTF_OpenFont("assets/arial.ttf", 24);
         if (font == nullptr) {
-            logErrorAndExit("TTF_OpenFont", TTF_GetError());
+            KiemtraLoi("TTF_OpenFont", TTF_GetError());
         }
 
 
         if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
-            logErrorAndExit("IMG_Init", IMG_GetError());
+            KiemtraLoi("IMG_Init", IMG_GetError());
         }
     }
 
-    void renderText(const string& text, int x, int y, SDL_Color color) {
-        SDL_Surface* surface = TTF_RenderText_Solid(font, text.c_str(), color);
+    void VeChuCai(const string& chu, int x, int y, SDL_Color mau) {
+        SDL_Surface* surface = TTF_RenderText_Solid(font, chu.c_str(), mau);
         SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-        SDL_Rect dest = {x, y, surface->w, surface->h};
-        SDL_RenderCopy(renderer, texture, nullptr, &dest);
+        SDL_Rect vitri = {x, y, surface->w, surface->h};
+        SDL_RenderCopy(renderer, texture, nullptr, &vitri);
         SDL_FreeSurface(surface);
         SDL_DestroyTexture(texture);
     }
 
-    void loadHangmanStages() {
+    void TaiHoatAnh() {
         for (int i = 0; i < 7; i++) {
             string path = "assets/hangman_" + to_string(i) + ".png";
             SDL_Surface* surface = IMG_Load(path.c_str());
             if (!surface) {
-                logErrorAndExit("IMG_Load", IMG_GetError());
+                KiemtraLoi("IMG_Load", IMG_GetError());
             }
-            hangmanStages[i] = SDL_CreateTextureFromSurface(renderer, surface);
+            HoatAnh[i] = SDL_CreateTextureFromSurface(renderer, surface);
             SDL_FreeSurface(surface);
         }
     }
 
-    void renderHangman(int stage) {
-        if (stage < 0 || stage >= 7) return;
-        SDL_Rect dest = {100, 300, 600, 388};
-        SDL_RenderCopy(renderer, hangmanStages[stage], nullptr, &dest);
+    void HienThiHoatAnh(int a) {
+        if (a < 0 || a >= 7) return;
+        SDL_Rect vitri = {100, 300, 600, 388};
+        SDL_RenderCopy(renderer, HoatAnh[a], nullptr, &vitri);
     }
 
     void quit() {
         for (int i = 0; i < 7; i++) {
-            if (hangmanStages[i]) {
-                SDL_DestroyTexture(hangmanStages[i]);
+            if (HoatAnh[i]) {
+                SDL_DestroyTexture(HoatAnh[i]);
             }
         }
         TTF_CloseFont(font);
@@ -77,7 +77,7 @@ struct Graphics {
         SDL_Quit();
     }
 
-    void logErrorAndExit(const char* msg, const char* error) {
+    void KiemtraLoi(const char* msg, const char* error) {
         SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "%s: %s", msg, error);
         SDL_Quit();
         exit(1);
