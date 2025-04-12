018b4802b50d179dff873c070b9d211f8fd93bc7
Vietanh279 <24020021@vnu.edu.vn>
2025-04-12 13:24:00 +0700

    Cauhinh.h

---
+++ Cauhinh.h
@@ -0,0 +1,14 @@
+#ifndef _CAUHINH_H
+#define _CAUHINH_H
+using namespace std;
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <string>
+#include <vector>
+
+
+const vector<string> CacTuTrongGame = {"HANGMAN", "PROGRAMMING", "SDL", "CODEBLOCKS", "GAME", "LTNC", "CODEGAME"};
+
+const int soLanDoanSaiToiDa = 6;
+
+#endif
